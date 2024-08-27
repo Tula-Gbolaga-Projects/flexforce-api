1155daa5cfc5d2301f10532fcc691a5df58953eb
Tula-Gbolaga-Fusion <gbolaga@reachcinema.io>
2024-08-27 03:34:33 +0100

    test

--- obj/Debug/net8.0/agency-portal-api.AssemblyInfo.cs
+++ obj/Debug/net8.0/agency-portal-api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("agency-portal-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f2fdc88356414d742d130e8ae97bc970a401358")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f562ee23d8312942fd4d3eba00812b5a592a8e9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("agency-portal-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("agency-portal-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
